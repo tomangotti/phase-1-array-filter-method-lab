b4a7d9938d3fb8d66c5d5043216d7dd243b61206
thomas <tom.angotti11@gmail.com>
2022-12-18 17:49:24 -0700

    commit tree cfa0ae48b07396e0fc9811e8013739113ad493fa

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+ function findMatching(collection, name) {
+    const matchingDrivers = collection.filter(function (user){
+        return name.toLowerCase() === user.toLowerCase();
+    })
+    return matchingDrivers
+ };
+
+
+console.log(findMatching(drivers, 'sammy'))
+
+function fuzzyMatch(collection, name) {
+    const matchingDrivers = collection.filter(function (user){
+        return name.slice(0,2) === user.slice(0,2);
+    })
+    return matchingDrivers
+}
+
+console.log(fuzzyMatch(drivers, "Sa"))
+
+function matchName(collection, driver) {
+    const matchingDrivers = collection.filter(function (user){
+        return driver === user.name; 
+    })
+    return matchingDrivers
+}
